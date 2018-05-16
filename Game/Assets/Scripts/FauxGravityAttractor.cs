db95efed36c1b10564a04e6fbffa29a015dcdea7
DESKTOP-FJB96GO\SamHuffer <101633177@student.swin.edu.au>
2018-05-16 20:29:07 +1000

    Trying to reconcile commit conflicts between faux gravity and spawn/split changes

---
+++ Game/Assets/Scripts/FauxGravityAttractor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityAttractor : MonoBehaviour {
+
+	public float gravity = -10;
+
+    public void Attract(Transform body) {
+		Vector3 gravityUp = (body.position - transform.position).normalized;
+		Vector3 bodyUp = body.up;
+
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+
+	}	
+}
