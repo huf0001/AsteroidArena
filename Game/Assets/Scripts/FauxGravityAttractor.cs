feef1080402034dd2bb7682780915326a27bf5d2
unknown <101655906@AS409-013.ds.swin.edu.au>
2018-05-17 17:29:50 +1000

    Updated gravity script

--- Game/Assets/Scripts/FauxGravityAttractor.cs
+++ Game/Assets/Scripts/FauxGravityAttractor.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FauxGravityAttractor : MonoBehaviour {
+public class FauxGravityAttractor : MonoBehaviour
+{
 
-	public float gravity = -10;
+    public float gravity = -10;
 
-    public void Attract(Transform body) {
-		Vector3 gravityUp = (body.position - transform.position).normalized;
-		Vector3 bodyUp = body.up;
+    public void Attract(Transform body)
+    {
 
-        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+        Vector3 gravityUp = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;
 
-
-	}	
+        Rigidbody rb = body.GetComponent<Rigidbody>();
+        float dist = Vector3.Distance(transform.position, body.position);
+        rb.AddForce((gravityUp * gravity) / (Mathf.Pow(dist, 2)));
+    }
 }
