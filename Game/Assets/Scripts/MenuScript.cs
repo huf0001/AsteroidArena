98b00ec152eeb7bf86db76907b34f51ed8779815
DESKTOP-FJB96GO\SamHuffer <101633177@student.swin.edu.au>
2018-05-19 20:56:25 +1000

    Name input for the leaderboard added to home screen (doesn't do anything yet); requires name to be entered to play, can't get it to turn red when the player tries to play without having entered their name. the text box doesn't seem to wanna go red.

--- Game/Assets/Scripts/MenuScript.cs
+++ Game/Assets/Scripts/MenuScript.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour 
 {
-	public void LoadScene(string sceneName)
+    public Image textbox;
+    public Text textDisplay;
+
+    public void LoadScene(string sceneName)
 	{
-		if (sceneName == null)
-			Debug.Log("<color=orange>"+gameObject.name+": No Scene Name Was given for LoadScene function!</color>");
-		SceneManager.LoadScene(sceneName); //load a scene
-	}
+        /*if (textDisplay.text == "")
+        {
+            textbox.color = new Color(255, 77, 77);
+            //ColorBlock cb = textbox.color;
+            //cb.normalColor = new Color(255, 120, 120);
+            //textbox.colors = cb;
+        }
+        else
+        {*/
+            if (sceneName == null)
+            {
+                Debug.Log("<color=orange>" + gameObject.name + ": No Scene Name Was given for LoadScene function!</color>");
+            }
+            SceneManager.LoadScene(sceneName); //load a scene
+        //}
+    }
 }
