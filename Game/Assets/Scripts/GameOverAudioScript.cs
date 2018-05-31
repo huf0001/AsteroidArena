3e1b648d3540894fa266cf3927e97979f80b8d0f
DESKTOP-FJB96GO\SamHuffer <101633177@student.swin.edu.au>
2018-05-31 15:32:30 +1000

    Got all the background music up and running, from the general music to the gravity music to the game over screen music for getting first place, completion, non-completion, etc.

--- Game/Assets/Scripts/GameOverAudioScript.cs
+++ Game/Assets/Scripts/GameOverAudioScript.cs
@@ -3,20 +3,66 @@ using System.Collections;
 
 public class GameOverAudioScript : MonoBehaviour
 {
-    public AudioClip gameOverSound;
+    public AudioClip standardAudio;
+    public AudioClip completionAudio;
+    public AudioClip topScoreAudio;
     private AudioSource gameOverSource;
+
+    private int playerProgress = 0;
+
+    private bool audioStarted = false;
 
     void Awake()
     {
         gameOverSource = this.gameObject.AddComponent<AudioSource>();
 
         gameOverSource.loop = true;
-        gameOverSource.playOnAwake = true;
+        gameOverSource.playOnAwake = false;
+    }
 
-        if (gameOverSound != null)
+    void Update()
+    {
+        if (audioStarted)
         {
-            gameOverSource.clip = gameOverSound;
-            gameOverSource.Play();
+            if (!gameOverSource.isPlaying)
+            {
+                if (playerProgress == 100)
+                {
+                    gameOverSource.clip = completionAudio;
+                }
+                else
+                {
+                    gameOverSource.clip = standardAudio;
+                }
+
+                gameOverSource.loop = true;
+                gameOverSource.Play();
+            }
+        }
+        else
+        {
+            if (PlayerPrefs.GetInt("saved") == 1)
+            {
+                int position = PlayerPrefs.GetInt("position");
+                playerProgress = PlayerPrefs.GetInt("progress");
+
+                if (position == 1)
+                {
+                    gameOverSource.clip = topScoreAudio;
+                    gameOverSource.loop = false;
+                }
+                else if (playerProgress == 100)
+                {
+                    gameOverSource.clip = completionAudio;
+                }
+                else
+                {
+                    gameOverSource.clip = standardAudio;
+                }
+
+                gameOverSource.Play();
+                audioStarted = true;
+            }
         }
     }
 }
